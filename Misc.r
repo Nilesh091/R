216d0092dc2d463485b8df34555ca6397c570a55
Nilesh Ranjan Sahu <nileshranjansahu@Nileshs-MacBook-Air-2.local>
2024-01-23 10:52:47 +0530

    Third commit

---
+++ Misc.r
@@ -0,0 +1,12 @@
+# a= 10:20
+# print(a)
+# a=c(20,30,40)
+# b=19
+# print(b %in% a)
+M=matrix(c(1,2,3,4,5,6),2,3,TRUE)
+N=matrix(c(1,2,3,4,5,6),2,3,FALSE)
+
+#print(M %*% t(M))
+# print(t(M)) 
+print(M)
+print(N)
--- logicalOperator.r
+++ logicalOperator.r
@@ -1,8 +1,8 @@
-a=0
-b=1
+a=4
+b=34
 print(a&b)
 print(a|b)
-print(a&&b)
+cat("&& :",a&&b,"\n")
 print(a||b)
 print(!a)
-print(!b)
+print(!b) 
